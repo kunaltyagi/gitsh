0dee97df49961abb54d953f3be03c5748de04cae
George Brocklehurst and Alex Topalov <george+alex@thoughtbot.com>
2014-02-28 10:16:43 +0100

    Fix tab completion for branches with a dot
    Completion after the dot would be seen as a new branch name. This is correct
    behaviour for two dots (e.g. HEAD~2..master) but not for one dot (e.g.
    fix-v1.4)

--- spec/units/completer_spec.rb
+++ spec/units/completer_spec.rb
@@ -2,77 +2,63 @@ require 'spec_helper'
 require 'gitsh/completer'
 
 describe Gitsh::Completer do
-  it 'completes commands and aliases' do
-    readline = stub('Readline', line_buffer: '')
-    env = stub(
-      'Environment',
+  let(:completer) { Gitsh::Completer.new(readline, env, internal_command) }
+
+  let(:readline) { stub('Readline', line_buffer: input) }
+  let(:env) do
+    stub('Environment', {
       git_commands: %w( stage stash status add commit ),
-      git_aliases: %w( adder )
-    )
-    internal_command = stub('InternalCommand', commands: %w( :set :exit ))
-    completer = Gitsh::Completer.new(readline, env, internal_command)
+      git_aliases: %w( adder ),
+      repo_heads: %w( master my-feature v1.0 fix-v1.5 fix-v1.5.1 fix-v1.5.2)
+    })
+  end
+  let(:internal_command) { stub('InternalCommand', commands: %w( :set :exit )) }
 
-    expect(completer.call('sta')).to eq ['stage ', 'stash ', 'status ']
-    expect(completer.call('ad')).to eq ['add ', 'adder ']
+  context 'when completing commands' do
+    let(:input) { '' }
+
+    it 'completes commands and aliases' do
+      expect(completer.call('sta')).to eq ['stage ', 'stash ', 'status ']
+      expect(completer.call('ad')).to eq ['add ', 'adder ']
+    end
+
+    it 'completes internal commands' do
+      expect(completer.call(':')).to eq [':set ', ':exit ']
+      expect(completer.call(':s')).to eq [':set ']
+    end
   end
 
-  it 'completes internal commands' do
-    readline = stub('Readline', line_buffer: '')
-    env = stub('Environment', git_commands: %w( stage stash ), git_aliases: [])
-    internal_command = stub('InternalCommand', commands: %w( :set :exit ))
-    completer = Gitsh::Completer.new(readline, env, internal_command)
+  context 'when completing arguments' do
+    let(:input) { 'checkout ' }
 
-    expect(completer.call(':')).to eq [':set ', ':exit ']
-    expect(completer.call(':s')).to eq [':set ']
-  end
+    it 'completes heads when a command has been entered' do
+      expect(completer.call('')).to include 'master ', 'my-feature ', 'v1.0 '
+      expect(completer.call('m')).to include 'master ', 'my-feature '
+      expect(completer.call('m')).not_to include 'v1.0 '
+    end
 
-  it 'completes heads when a command has been entered' do
-    readline = stub('Readline', line_buffer: 'checkout ')
-    env = stub('Environment', repo_heads: %w( master my-feature v1.0 ))
-    internal_command = stub('InternalCommand')
-    completer = Gitsh::Completer.new(readline, env, internal_command)
+    it 'completes head when branch include a dot' do
+      expect(completer.call('fix-v1.')).to include 'fix-v1.5 ', 'fix-v1.5.1 ', 'fix-v1.5.2 '
+    end
 
-    expect(completer.call('')).to include 'master ', 'my-feature ', 'v1.0 '
-    expect(completer.call('m')).to include 'master ', 'my-feature '
-    expect(completer.call('m')).not_to include 'v1.0 '
-  end
+    it 'completes heads starting with :' do
+      expect(completer.call('master:m')).to include 'master:my-feature '
+    end
 
-  it 'completes paths beginning with a ~ character' do
-    readline = stub('Readline', line_buffer: ':cd ')
-    env = stub('Environment', repo_heads: %w( master ))
-    internal_command = stub('InternalCommand')
-    completer = Gitsh::Completer.new(readline, env, internal_command)
+    it 'ignores input before punctuation when completing heads' do
+      expect(completer.call('mas:')).to include 'mas:master ', 'mas:my-feature '
+    end
 
-    expect(completer.call('~/')).to include "#{first_regular_file('~')} "
-  end
+    it 'completes paths beginning with a ~ character' do
+      expect(completer.call('~/')).to include "#{first_regular_file('~')} "
+    end
 
-  it 'completes paths containing .. and .' do
-    project_root = File.expand_path('../../../', __FILE__)
-    path = File.join(project_root, 'spec/./units/../units')
-    readline = stub('Readline', line_buffer: ':cd ')
-    env = stub('Environment', repo_heads: %w( master ))
-    internal_command = stub('InternalCommand')
-    completer = Gitsh::Completer.new(readline, env, internal_command)
+    it 'completes paths containing .. and .' do
+      project_root = File.expand_path('../../../', __FILE__)
+      path = File.join(project_root, 'spec/./units/../units')
 
-    expect(completer.call("#{path}/")).to include "#{first_regular_file(path)} "
-  end
-
-  it 'completes heads starting with :' do
-    readline = stub('Readline', line_buffer: 'push ')
-    env = stub('Environment', repo_heads: %w( master hello-branch ))
-    internal_command = stub('InternalCommand')
-    completer = Gitsh::Completer.new(readline, env, internal_command)
-
-    expect(completer.call('master:h')).to include 'master:hello-branch '
-  end
-
-  it 'ignores input before punctuation when completing heads' do
-    readline = stub('Readline', line_buffer: 'push ')
-    env = stub('Environment', repo_heads: %w( master ))
-    internal_command = stub('InternalCommand')
-    completer = Gitsh::Completer.new(readline, env, internal_command)
-
-    expect(completer.call('mas:')).to eq [ 'mas:master ' ]
+      expect(completer.call("#{path}/")).to include "#{first_regular_file(path)} "
+    end
   end
 
   def first_regular_file(directory)
